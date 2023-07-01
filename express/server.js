c9ea029c9e879bca327129cc764024ccaf3cd05c
AnnaGSamuel <annagsamuel3002@gmail.com>
2023-07-01 08:34:58 +0530

    added features

--- express/server.js
+++ express/server.js
@@ -5,7 +5,6 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-//app.set('views'+ __dirname + '/views');
 
 // MongoDB connection string and database name
 const mongoURI = 'mongodb://127.0.0.1:27017';
@@ -13,7 +12,6 @@ const dbName = 'library';
 
 // Middleware for parsing JSON requests
 app.use(express.json());
-
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
@@ -32,6 +30,7 @@ const client =  new MongoClient(mongoURI, {
   }
 })
 
+// Search for a book or journal by name
 const search = async (query) => {
   try {
     await client.connect().catch(console.error);
@@ -53,16 +52,24 @@ const search = async (query) => {
     await client.close();
   }
 }
-  // Search for a book or journal by name
-app.get('/search', (req, res) => {
+app.get('/searchBooks', (req, res) => {
     const query = req.query.searchInput;
 
     console.log(query);
     search(query).then(
-      results => results ? res.json(results) : res.status(500).json({ error: 'Internal server error' })
+      results => results ? res.render("booksSearch" , {results} ) : res.status(500).json({ error: 'Internal server error' })
     )
   }); 
- 
+  app.get('/searchJournals', (req, res) => {
+    const query = req.query.searchInput;
+
+    console.log(query);
+    search(query).then(
+      results => results ? res.render("journalsSearch" , {results} ) : res.status(500).json({ error: 'Internal server error' })
+    )
+  }); 
+  
+   // Search for books or journals by publication date
   const searchByDate = async (date) => {
     try {
       await client.connect().catch(console.error);
@@ -84,17 +91,21 @@ app.get('/search', (req, res) => {
       await client.close();
     }
   }
-
-  // Search for books or journals by publication date
-  app.get('/searchByDate', (req, res) => {
+  app.get('/searchByDateBooks', (req, res) => {
     const date = req.query.dateInput;
     searchByDate(date).then(
-      results => { results ? res.json(results) : res.status(500).json({ error: 'Internal server error' })
+      results => { results ? res.render("booksByDate" , {date, results} ) : res.status(500).json({ error: 'Internal server error' })  //res.json(results)
+  }) 
+  });
+  app.get('/searchByDateJournals', (req, res) => {
+    const date = req.query.dateInput;
+    searchByDate(date).then(
+      results => { results ? res.render("journalsByDate" , {date, results} ) : res.status(500).json({ error: 'Internal server error' })  //res.json(results)
   }) 
   });
   
   // Update the name of a book or journal
-  app.get('/update', (req, res) => {
+  app.get('/updateBooks', (req, res) => {
     const currID = req.query.prev;
     const newName = req.query.updateInput;
   
@@ -106,7 +117,11 @@ app.get('/search', (req, res) => {
         collection.updateOne({ id: currID }, { $set: { name: newName } }) 
           .then(result => {
             if (result.modifiedCount === 1) {
-              res.json({ message: 'Update successful' });
+              //res.json({ message: 'Update successful' });  window.location.href = '/'; // Redirect to homepage or desired page
+              const response = ` <script>
+                alert('Update successful');
+              </script> `;
+            res.send(response);
             } else {
               res.status(404).json({ error: 'Entity not found' });
             }
@@ -122,22 +137,45 @@ app.get('/search', (req, res) => {
       });
   });
   
+  app.get('/updateJournals', (req, res) => {
+    const currID = req.query.prev;
+    const newName = req.query.updateInput;
+  
+    MongoClient.connect(mongoURI, { useUnifiedTopology: true })
+      .then(client => {
+        const db = client.db(dbName);
+        const collection = db.collection('journals');
+  
+        collection.updateOne({ id: currID }, { $set: { name: newName } }) 
+          .then(result => {
+            if (result.modifiedCount === 1) {
+              const response = ` <script>
+                alert('Update successful');
+              </script> `;
+            res.send(response);
+            } else {
+              res.status(404).json({ error: 'Entity not found' });
+            }
+          })
+          .catch(error => {
+            console.error('Error updating entity:', error);
+            res.status(500).json({ error: 'Internal server error' });
+          });
+      })
+      .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      });
+  });
+  
+  // Generate a report on the list of books and journals
   const report1 = async () => {
     try {
       await client.connect();
       const db = client.db(dbName);
       const booksCollection = db.collection('books');
       const results = {}
-      results.books = await booksCollection.aggregate([
-        {
-          $lookup: {
-            from: 'authors',
-            localField: 'authorId',
-            foreignField: '_id',
-            as: 'authors'
-          }
-        }
-      ]).toArray().catch(console.error)
+      results.books = await booksCollection.aggregate().toArray().catch(console.error)
       return results;
     } catch(e) {
       console.error(e);
@@ -145,30 +183,19 @@ app.get('/search', (req, res) => {
       await client.close();
     }
   }
-
-  // Generate a report on the list of books and journals
   app.get('/reportBooks', (req, res) => {
     report1().then(
-      results => results ?  res.json(results) : res.status(500).json({ error: 'Internal server error' })
+      results => results ?  res.render("booksReport" , {results} ) : res.status(500).json({ error: 'Internal server error' })
     ) 
   });
-  
+
   const report2 = async () => {
     try {
       await client.connect();
       const db = client.db(dbName);
       const journalsCollection = db.collection('journals');
       const results = {}
-      results.journals = await journalsCollection.aggregate([
-        {
-          $lookup: {
-            from: 'authors',
-            localField: 'authorId',
-            foreignField: '_id',
-            as: 'authors'
-          }
-        }
-      ]).toArray().catch(console.error)
+      results.journals = await journalsCollection.aggregate().toArray().catch(console.error)
       return results;
     } catch(e) {
       console.error(e);
@@ -176,9 +203,8 @@ app.get('/search', (req, res) => {
       await client.close();
     }
   }
-
   app.get('/reportJournals', (req, res) => {
     report2().then(
-      results => results ?  res.json(results) : res.status(500).json({ error: 'Internal server error' })
+      results => results ?  res.render("journalsReport" , {results} ) : res.status(500).json({ error: 'Internal server error' })
     ) 
   });
